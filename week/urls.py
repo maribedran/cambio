fe52848082a69a4d41eb7817a6fc49bbf6ce21a3
mari <maribedran@gmmail.com>
2014-11-21 18:22:31 -0200

    Cria camihos pra dolar, euro e peso

--- week/urls.py
+++ week/urls.py
@@ -2,12 +2,16 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
+from home import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'week.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', include('week.home.urls', namespace='home')),
+    url(r'^$', views.dolar, name='dolar'),
+    url(r'^dolar/$', views.dolar, name='dolar'),
+    url(r'^euro/$', views.euro, name='euro'),
+    url(r'^peso/$', views.peso, name='peso'),
     url(r'^admin/', include(admin.site.urls)),
 )
