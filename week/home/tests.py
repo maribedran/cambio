0aa772d1a3df8492bec6d9dd7f655615f72814c2
mari <maribedran@gmmail.com>
2014-11-21 18:28:05 -0200

    Todo o resto. Malzaê, futuro!

--- week/home/tests.py
+++ week/home/tests.py
@@ -12,6 +12,14 @@ class HomeTest(TestCase):
 		'Response must render template'
 		self.assertTemplateUsed(self.response, 'home/index.html')
 
-	def test_context(self):
-		'Rates must be in context'
-		self.assertEqual(self.response.context['2014-11-14'], 2.6059)
+	# def test_context(self):
+	# 	'Rates must be in context'
+	# 	self.assertEqual(self.response.context, 'week_rates')
+
+class DateTest(TestCase):
+	def setUp(self):
+		self.context = self.client.get('/').context
+
+	def test_datetime(self):
+		self.assertEqual(len(self.context['week_rates'].keys()), 7)
+		
--- week/home/views.py
+++ week/home/views.py
@@ -1,21 +1,55 @@
 # coding: utf-8
 from django.shortcuts import render
 import requests
+from datetime import date, timedelta
+import collections
 
-response = requests.get('http://jsonrates.com/historical/?'+
-	'from=USD'+
-	'&to=BRL'+
-    '&dateStart=2014-11-14'+
-    '&dateEnd=2014-11-20'
-)
+today = date.today()
+last_week = today - timedelta(7)
 
-json = response.json()
+def get_currency(currency):
+	response = requests.get('http://jsonrates.com/historical/?'+
+		'from='+
+		currency+
+		'&to=BRL'+
+	    '&dateStart='+ str(last_week)+
+	    '&dateEnd='+ str(today)
+	)
 
-week_rates = {}
+	json = response.json()
 
-for date, values in json['rates'].iteritems():
-    histrate = float(values['rate'])
-    week_rates[date] = histrate
+	week_rates = {}
 
-def index(request):
- return render(request, 'home/index.html', week_rates)
+	for date, values in json['rates'].iteritems():
+	    histrate = round(float(values['rate']), 2)
+	    utc_date = values['utctime']
+	    week_rates[utc_date] = histrate
+
+	week_rates = collections.OrderedDict(sorted(week_rates.items()))
+
+	return week_rates
+
+def get_name(currency):
+	if currency == 'USD':
+		currency_name = 'Dólar'
+	elif currency == 'EUR':
+		currency_name = 'Euro'
+	elif currency == 'ARS':
+		currency_name = 'Pesos argentinos'
+
+	return currency_name
+
+def dolar(request):
+	week_rates = get_currency('USD')
+	currency_name = get_name('USD')
+	return render(request, 'home/index.html', {'week_rates' : week_rates, 'currency_name' : currency_name})
+
+def euro(request):
+	week_rates = get_currency('EUR')
+	currency_name = get_name('EUR')
+	return render(request, 'home/euro.html', {'week_rates' : week_rates, 'currency_name' : currency_name})
+
+def peso(request):
+	week_rates = get_currency('ARS')
+	currency_name = get_name('ARS')
+	return render(request, 'home/peso.html', {'week_rates' : week_rates, 'currency_name' : currency_name})
